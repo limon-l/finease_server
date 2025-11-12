46211bd6942aae3227437034401c59d441c965d5
limon-l <limonroyapu.edu@gmail.com>
2025-11-13 03:24:13 +0600

    err fixed

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const app = express();
 const PORT = 5000;
 connectDB();
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 // Middleware
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
